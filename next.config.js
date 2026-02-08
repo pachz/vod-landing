1b375d8ff85ab475be4156767f918b1482ca5573
Payam Chamani <pchamani@nizek.com>
2026-02-08 22:56:41 +0330

    hm

--- next.config.js
+++ next.config.js
@@ -4,12 +4,6 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'impartial-jellyfish-472.convex.cloud',
         port: '',
         pathname: '/**',
